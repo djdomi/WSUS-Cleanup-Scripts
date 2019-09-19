1d58d29bcc00536894d42d90cde0037adc75f118
DoMeLe <1812020+djdomi@users.noreply.github.com>
2019-09-19 08:31:41 +0200

    initial update
    these scripts are NOT mine, i only submit them to share

---
+++ Cleanup-WSUSserver.ps1
@@ -0,0 +1,18 @@
+﻿$server = ''
+$port = ''
+Write-Progress -Activity 'Getting WSUS server'
+$WSUSserver = Get-WsusServer -Name $server -PortNumber $port
+Write-Progress -Activity 'Getting approved updates, this may take a while...' -PercentComplete -1
+$approvedupdates = Get-WsusUpdate -UpdateServer $WSUSserver -Approval Approved -Status InstalledOrNotApplicableOrNoStatus
+Write-Progress -Activity 'Retrieved updates' -PercentComplete 90
+$i = 0
+$superseded = $approvedupdates | ? {$_.Update.IsSuperseded -eq $true -and $_.ComputersNeedingThisUpdate -eq 0}
+$total = $superseded.count
+foreach ($update in $superseded)
+{
+    Write-Progress -Activity 'Declining updates' -Status "$($update.Update.Title)" -PercentComplete (($i/$total) * 100)
+    $update.Update.Decline()
+    $i++
+}
+
+Write-Host "Total declined updates: $total" -ForegroundColor Yellow
---
+++ wsus-clean-drivers.ps1
@@ -0,0 +1,3 @@
+﻿[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer();
+$wsus.GetUpdates() | Where {$_.UpdateClassificationTitle -eq 'Drivers'} | ForEach-Object {$wsus.DeleteUpdate($_.Id.UpdateId.ToString()); Write-Host $_.Title removed }
